6d06449831b841798a894d71e27bf9a2fa7bf612
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-05-02 23:00:36 +0900

    나도코딩 9-7 메소드 오버라이딩

--- 2313_practice.py
+++ 2313_practice.py
@@ -738,16 +738,70 @@ print("키 {0}cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender
 
 # 다중상속
 # 일반 유닛
+# class Unit:
+#     def __init__(self, name, hp):
+#         self.name = name
+#         self.hp = hp
+#
+#
+# # 공격 유닛
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, damage):
+#         Unit.__init__(self, name, hp)
+#         self.damage = damage
+#
+#     def attack(self, location):
+#         print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]" \
+#               .format(self.name, location, self.damage))
+#
+#     def damaged(self, damage):
+#         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+#         self.hp -= damage
+#         print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+#         if self.hp <= 0:
+#             print("{0} : 파괴되었습니다.".format(self.name))
+#
+#
+# # 날 수 있는 기능을 가진 클래스
+# class Flyable:
+#     def __init__(self, flying_speed):  # 공중 이동 속도
+#         self.flying_speed = flying_speed
+#
+#     def fly(self, name, location):  # 유닛 이름, 이동 방향
+#         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]" \
+#               .format(name, location, self.flying_speed))
+#
+#
+# # 공중 공격 유닛
+# class FlyableAttackUnit(AttackUnit, Flyable):
+#     def __init__(self, name, hp, damage, flying_speed):  # 이름, 체력, 공격력, 공중 이동 속도
+#         AttackUnit.__init__(self, name, hp, damage)  # 이름, 체력, 공격력
+#         Flyable.__init__(self, flying_speed)  # 공중 이동 속도
+#
+#
+# # 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+# valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)  # 이름, 체력, 공격력, 공중 이동 속도
+# valkyrie.fly(valkyrie.name, "3시")  # 3시 방향으로 발키리를 이동
+
+
+# 메소드 오버라이딩
+# 일반 유닛
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]" \
+              .format(self.name, location, self.speed))
 
 
 # 공격 유닛
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
-        Unit.__init__(self, name, hp)
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name, hp, speed)
         self.damage = damage
 
     def attack(self, location):
@@ -764,21 +818,31 @@ class AttackUnit(Unit):
 
 # 날 수 있는 기능을 가진 클래스
 class Flyable:
-    def __init__(self, flying_speed):  # 공중 이동 속도
+    def __init__(self, flying_speed):
         self.flying_speed = flying_speed
 
-    def fly(self, name, location):  # 유닛 이름, 이동 방향
+    def fly(self, name, location):
         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]" \
               .format(name, location, self.flying_speed))
 
 
 # 공중 공격 유닛
 class FlyableAttackUnit(AttackUnit, Flyable):
-    def __init__(self, name, hp, damage, flying_speed):  # 이름, 체력, 공격력, 공중 이동 속도
-        AttackUnit.__init__(self, name, hp, damage)  # 이름, 체력, 공격력
-        Flyable.__init__(self, flying_speed)  # 공중 이동 속도
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, 0, damage)
+        Flyable.__init__(self, flying_speed)
+
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
 
 
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
-valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)  # 이름, 체력, 공격력, 공중 이동 속도
-valkyrie.fly(valkyrie.name, "3시")  # 3시 방향으로 발키리를 이동
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit("벌쳐", 80, 10, 20)  # 지상 speed 10
+
+# 배틀크루저 : 공중 유닛, 체력도 굉장히 좋음, 공격력도 좋음
+battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
+
+vulture.move("11시")
+# battlecruiser.fly(battlecruiser.name, "9시")
+battlecruiser.move("9시")  # 오버라이딩된 move() 호출
