523a6fdf5224abf1a4084d29640e927ab4111e61
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-24 09:33:28 +0900

    custom-error 생성, raise

--- module, packacge/baseball.py
+++ module, packacge/baseball.py
@@ -1,9 +1,17 @@
 from baseball_game_engine import make_quiz, check
+from custom_error import InvalidCountError
 answer = make_quiz()
 # 무한반복
 while True:
 #   숫자 3자리 중복 없이 묻자
     player = input("숫자 세자리는? : ")
+    try: # 숫자가 아닐 때 에러 처리
+        player_int = int(player)
+    except ValueError:
+        continue
+    # 길이가 3이 아닐 때 에러 처리
+    if len(player) != 3:
+        raise InvalidCountError("3자리가 아닙니다.")
 #   Strike, ball 확인하자
     strike, ball = check(answer, player)
 #   출력하자
