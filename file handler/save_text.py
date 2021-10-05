09137911f9b9214bc85b1d3c2e399db2d9e5b6af
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-10-05 11:08:08 +0900

    save_text

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,10 +1,13 @@
-try:
-    f = open('text.txt', 'w', encoding='utf-8')
+f = open('text.txt', 'w', encoding='utf-8')
 
+f.write('hello')
+f.write('\n')
+f.write('월드')
+
+f.close()
+
+print('with')
+with open('text.txt', 'w', encoding='utf-8') as f:
     f.write('hello')
     f.write('\n')
     f.write('월드')
-except:
-    pass
-finally:
-    f.close()
